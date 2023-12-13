35b03710d77c62c0466539c13225d55fea268f47
Nicolás <nicolasvits19@gmail.com>
2023-12-12 22:45:17 -0500

    arreglos finales

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,22 +1,20 @@
-import React from "react";
+import info from "../../assets/img/info.png";
 
 const Info = () => {
   return (
-    <div className="image-grid">
+    <div className="grid">
       <div className="info-container">
-        <div className="info-text">
-          <h1>Bienvenido a Social Lab: Impulsando el Cambio a través de la Tecnología</h1>
-          <h2>OBJETIVO ESTRATEGICO</h2>
-          <p>
-            Nos enorgullece facilitar el acceso tecnológico a organizaciones sin fines de lucro y fundaciones.
-            Alineamos acciones con la estrategia empresarial, contribuyendo a la automatización, creación de páginas web y mejora de plataformas.
-            Nuestro propósito es apoyar iniciativas alineadas con los Objetivos de Desarrollo Sostenible (ODS).
-            Garantizamos que la tecnología sea accesible y efectiva para quienes trabajan incansablemente por mejorar el mundo.
-          </p>
-        </div>
-        <div className="image-container">
-          imagen
-        </div>
+        <h1>Bienvenido a Social Lab: Impulsando el Cambio a través de la Tecnología</h1>
+        <h2>OBJETIVO ESTRATEGICO</h2>
+        <p>
+          Nos enorgullece facilitar el acceso tecnológico a organizaciones sin fines de lucro y fundaciones.
+          Alineamos acciones con la estrategia empresarial, contribuyendo a la automatización, creación de páginas web y mejora de plataformas.
+          Nuestro propósito es apoyar iniciativas alineadas con los Objetivos de Desarrollo Sostenible (ODS).
+          Garantizamos que la tecnología sea accesible y efectiva para quienes trabajan incansablemente por mejorar el mundo.
+        </p>
+      </div>
+      <div className="image-container">
+        <img  src={info} alt="imagen representativa de informacion"/>
       </div>
     </div>
   );
