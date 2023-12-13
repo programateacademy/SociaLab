c9e10e7259f7d6b4662c0bbd0e98e404481576da
Jair De Jesus Uribe Valencia <xdreyveringx@hotmail.com>
2023-12-13 18:31:00 -0500

    fit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Ods from './pages/odsDetail/Ods';
 import Home from './pages/home/home/Home';
+import Portfolio from './components/portfolio/Portfolio';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Portfolio />} />
         <Route path="/ods/:id" element={<Ods />} />
       </Routes>
     </Router>
