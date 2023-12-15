5b0c68407df3d177c17b708e2a79b8e376bfabc3
Jair De Jesus Uribe Valencia <xdreyveringx@hotmail.com>
2023-12-15 17:20:58 -0500

    merge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Ods from './pages/odsDetail/Ods';
 import Portfolio from './components/portfolio/Portfolio';
+import Form from './components/form/Form';
 
 function App() {
   return (
     <Router>
+      <Form />
       <Routes>
         <Route path="/" element={<Portfolio />} />
         <Route path="/ods/:id" element={<Ods />} />
--- src/pages/home/odsDetail/Ods.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-const Ods = () => {
-  return (
-    <div>Ods</div>
-  )
-}
-export default Ods
