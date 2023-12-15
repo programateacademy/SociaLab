1d9a831f32e5079cb6b8b34091c52fd0a0de5fe1
SXNTYACEROA104527 <santyacero457@hotmail.com>
2023-12-15 18:51:09 -0500

    fix styles and logic email

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -16,39 +16,47 @@ const Form = () => {
       });
   };
 
+  const sended = () => {
+    alert('Formulario enviado')
+  }
+
   return (
-    <div className='container'>
-      <form ref={form} onSubmit={sendEmail} className='field'>
+    <div className='container' id='form'>
+      <div>
+        <h2 className='tituloForm'>Construyendo un futuro sostenible con tecnología accesible</h2>
+        <p className='parrafoForm'>Forma parte del movimiento: Facilitamos tecnología para organizaciones sin fines de lucro y fundaciones. ¡Completa el formulario y únete a nuestra misión de transformar el acceso tecnológico!</p>
+      </div>
+      <form className='containerForm' ref={form} onSubmit={sendEmail} >
 
-        <input className='cuadros' type="text" name="org_name" placeholder='Nombre  de la empresa / organización'/>
+        <input className='input' type="text" name="org_name" placeholder='Nombre  de la empresa / organización'/>
 
-        <input className='cuadros' type="text" name="org_project_approach" placeholder='Enfoque o proyecto actual de empresa u organización' />
+        <input className='input' type="text" name="org_project_approach" placeholder='Enfoque o proyecto actual de empresa u organización' />
         
-        <input className='cuadros' type="text" name="user_name" placeholder='Nombre del responsable del proyecto'/>
+        <input className='input' type="text" name="user_name" placeholder='Nombre del responsable del proyecto'/>
 
-        <input className='cuadros' type="text" name="user_phone" placeholder='Teléfono de contacto del responsable'/>
+        <input className='input' type="text" name="user_phone" placeholder='Teléfono de contacto del responsable'/>
 
-        <input className='cuadros' type="email" name="user_email" placeholder='Email  de contacto del responsable'/>
+        <input className='input' type="email" name="user_email" placeholder='Email  de contacto del responsable'/>
 
-        <select name="multiple1" >
-          <option selected disabled >Status actual de la página web:</option>
-          <option value="Diseño web desde cero">Diseño web desde cero</option>
-          <option value="Diseño web nuevo a partir de una página web existente">Diseño web nuevo a partir de una página web existente</option>
+        <select name="multiple1" className='select'>
+          <option selected disabled>Status actual de la página web:</option>
+          <option className='option' value="Diseño web desde cero">Diseño web desde cero</option>
+          <option className='option' value="Diseño web nuevo a partir de una página web existente">Diseño web nuevo a partir de una página web existente</option>
         </select>
 
-        <select name="multiple2" id="lang" >
+        <select name="multiple2"className='select'>
           <option selected disabled >Tipo de desarrollo:</option>
-          <option value="Creación de sitio web corporativo con registro de usuarios con roles asignados">Creación de sitio web corporativo con registro de usuarios con roles asignados</option>
-          <option value="Creación de landing page para recolección de fondos">Creación de landing page para recolección de fondos</option>
-          <option value="Creación de un sistema intranet extranet (Intranet: Es una plataforma a la que únicamente pueden acceder los propios empleados de la compañía. Extranet: Cuando se puede acceder a ella a través de Internet por parte de usuarios externos.)">Creación de un sistema intranet extranet (Intranet: Es una plataforma a la que únicamente pueden acceder los propios empleados de la compañía. Extranet: Cuando se puede acceder a ella a través de Internet por parte de usuarios externos.)</option>
-          <option value="Crear una tienda online eCommerce (tienda online de productos o servicios)">Crear una tienda online eCommerce (tienda online de productos o servicios)</option>
-          <option value="Creacion de  web educativa">Creacion de  web educativa</option>
-          <option value="Creacion de  sitio web para seguimiento de procesos">Creacion de  sitio web para seguimiento de procesos</option>
+          <option className='option' value="Creación de sitio web corporativo con registro de usuarios con roles asignados">Creación de sitio web corporativo con registro de usuarios con roles asignados</option>
+          <option className='option' value="Creación de landing page para recolección de fondos">Creación de landing page para recolección de fondos</option>
+          <option className='option' value="Creación de un sistema intranet extranet (Intranet: Es una plataforma a la que únicamente pueden acceder los propios empleados de la compañía. Extranet: Cuando se puede acceder a ella a través de Internet por parte de usuarios externos.)">Creación de un sistema intranet extranet (Intranet: Es una plataforma a la que únicamente pueden acceder los propios empleados de la compañía. Extranet: Cuando se puede acceder a ella a través de Internet por parte de usuarios externos.)</option>
+          <option className='option' value="Crear una tienda online eCommerce (tienda online de productos o servicios)">Crear una tienda online eCommerce (tienda online de productos o servicios)</option>
+          <option className='option' value="Creacion de  web educativa">Creacion de  web educativa</option>
+          <option className='option' value="Creacion de  sitio web para seguimiento de procesos">Creacion de  sitio web para seguimiento de procesos</option>
         </select>
 
-        <textarea name="message" placeholder='Cuentanos del proyecto y lo que requiere.' />
+        <textarea className='textarea' name="message" placeholder='Cuentanos del proyecto y lo que requiere.' />
 
-        <button type="submit" value="Send"> Send </button> 
+        <button className='button' type="submit" value="Send" onClick={sended}> Enviar </button> 
       </form>
     </div>
   );
