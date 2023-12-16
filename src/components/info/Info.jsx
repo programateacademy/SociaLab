d846d9d2b4d3f9bc5457408d30a84821ac2efd33
Nicolás <nicolasvits19@gmail.com>
2023-12-15 20:53:52 -0500

    codigo actual

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -7,9 +7,9 @@ const Info = () => {
       <h1 className="nosotros">SOBRE NOSOTROS</h1>
       <div className="grid">
         <div className="info-container">
-          <p>Bienvenido a Social Lab: Impulsando el Cambio a través de la Tecnología</p>
+          <p className="parrafo" >Bienvenido a Social Lab: Impulsando el Cambio a través de la Tecnología</p>
           <h2 className="obj">OBJETIVO ESTRATEGICO</h2>
-          <p>
+          <p className="parrafo">
             Nos enorgullece facilitar el acceso tecnológico a organizaciones sin fines de lucro y fundaciones;
             contribuyendo a la automatización, creación de páginas web y mejora de plataformas.
             Nuestro propósito es apoyar iniciativas alineadas con los Objetivos de Desarrollo Sostenible (ODS).
