61a428c41dab8143ed86af28bc119b64c95e4bd8
Nicolás <nicolasvits19@gmail.com>
2023-12-19 20:56:30 -0500

    arreglo top

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -32,12 +32,12 @@ const Info = () => {
               alt="imagen representativa de informacion"
             />
           </div>
-        </div>
-        <img
+          <img
           className="homme"
           src={Homme}
           alt="imagen representativa de informacion"
         />
+        </div>
       </div>
     </>
   );
