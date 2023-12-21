858280931386922fc1a1de0a8a8446d86cc6d3ca
shaleyma <shaleymaamell2006@gmail.com>
2023-12-20 20:23:41 -0500

    Home actualizacion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Home from "./pages/home/home/Home"
-import Navbar from "./components/navbar/Navbar";
 const App = () => {
   return (
     <>
-    <Navbar/>
       <Home/>
     </>
   );
--- src/data/CarrouselData.js
+++
@@ -1,8 +0,0 @@
-export const CarrouselData = [
-    {
-      id: 1, imgUrl: "./img/Banner1.png"
-    },
-    {
-      id: 2, imgUrl: "./img/Banner2.png"
-    }
-  ];
