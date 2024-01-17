288a482a9412363d28cdc02a9d19909a62b76d1f
SXNTYACEROA104527 <santyacero457@hotmail.com>
2024-01-16 23:40:23 -0500

    Fix responsive

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -33,34 +33,34 @@ const Form = () => {
   })*/
 
   return (
-    <div className='formCenter'>
-      <div className='containerFatherForm' id='form'>
-        <div className='containerText'>
-          <h2 className='titleForm'>Construyendo un futuro sostenible con tecnología accesible</h2>
-          <p className='pForm'>Forma parte del movimiento: Facilitamos tecnología para organizaciones sin fines de lucro y fundaciones. ¡Completa el formulario y únete a nuestra misión de transformar el acceso tecnológico!</p>
-        </div>
+    <div className='containerFatherForm' id='form'>
+      <div className='containerText'>
+        <h2 className='titleForm'>Construyendo un futuro sostenible con tecnología accesible</h2>
+        <p className='pForm'>Forma parte del movimiento: Facilitamos tecnología para organizaciones sin fines de lucro y fundaciones. ¡Completa el formulario y únete a nuestra misión de transformar el acceso tecnológico!</p>
+      </div>
 
-        <div className='line'> <img src={lineForm} alt="."/></div>
+      <div className='line'> <img src={lineForm} alt="."/></div>
 
+      <div>
         <form className='containerForm' ref={form} onSubmit={sendEmail} id='reset' >
 
-          <input className='input' type="text" name="org_name" placeholder='Nombre de la empresa / organización'/>
+          <input className='input locI1' type="text" name="org_name" placeholder='Nombre de la empresa / organización'/>
 
-          <input className='input' type="text" name="org_project_approach" placeholder='Enfoque o proyecto actual de la organización' />
+          <input className='input locI2' type="text" name="org_project_approach" placeholder='Enfoque o proyecto actual de la organización' />
           
-          <input className='input' type="text" name="user_name" placeholder='Nombre del responsable del proyecto'/>
+          <input className='input locI3' type="text" name="user_name" placeholder='Nombre del responsable del proyecto'/>
 
-          <input className='input' type="number" name="user_phone" placeholder='Teléfono de contacto del responsable'/>
+          <input className='input locI4' type="number" name="user_phone" placeholder='Teléfono de contacto del responsable'/>
 
-          <input className='input' type="email" name="user_email" placeholder='Email  de contacto del responsable'/>
+          <input className='input locI5' type="email" name="user_email" placeholder='Email  de contacto del responsable'/>
 
-          <select name="multiple1" className='select'>
+          <select name="multiple1" className='select locSelect1'>
             <option selected disabled>Status actual de la página web:</option>
             <option className='option' value="Diseño web desde cero">Diseño web desde cero</option>
             <option className='option' value="Diseño web nuevo a partir de una página web existente">Diseño web nuevo a partir de una página web existente</option>
           </select>
 
-          <select name="multiple2"className='select'>
+          <select name="multiple2"className='select .locSelect2'>
             <option selected disabled >Tipo de desarrollo:</option>
             <option className='option' value="Creación de sitio web corporativo con registro de usuarios con roles asignados">Creación de sitio web corporativo con registro de usuarios</option>
             <option className='option' value="Creación de landing page para recolección de fondos">Creación de landing page para recolección de fondos</option>
@@ -71,8 +71,9 @@ const Form = () => {
           </select>
 
           <textarea className='textarea' name="message" placeholder='Cuentanos del proyecto y lo que requiere.' />
-
-          <button className='button' type="submit" value="Send" onClick={sended}> Enviar </button> 
+          
+          <button className='button' type="submit" value="Send" onClick={sended}> Enviar </button>
+ 
         </form>
       </div>
     </div>
